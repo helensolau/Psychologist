cae8f00cb0d675db9ded1519202451cd645a6ce6
Helen Lau <hlau@lyft.com>
2016-07-05 18:00:57 -0700

    Psychologist

---
+++ Psychologist/HapinessViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HapinessViewController.swift
+//  Happiness
+//
+//  Created by Helen Lau on 6/30/16.
+//  Copyright © 2016 Helen Lau. All rights reserved.
+//
+
+import UIKit
+
+class HapinessViewController: UIViewController, FaceViewDataSource {
+    
+    var happiness: Int = 75 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended:
+            fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default:
+            break
+        }
+    }
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.scale)))
+        }
+    }
+    
+    private func updateUI() {
+        faceView?.setNeedsDisplay()
+        title = "\(happiness)"
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+}
